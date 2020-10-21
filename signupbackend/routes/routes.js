71818e89327c208265af3be84133cb50957759b3
JAY-Kittisak <71874439+JAY-Kittisak@users.noreply.github.com>
2020-10-21 17:23:06 +0700

    commit pages and componant

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -25,5 +25,22 @@ router.post('/signup', async (request, response) => {
         })
 })
 
+router.get("/profile", (req, res) => {
+  const signedUpUser = new signUpTemplateCopy({
+    fullName: request.body.fullName,
+    username: request.body.username,
+    email: request.body.email,
+    password: securePassword,
+  });
+  signedUpUser
+    .save()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+});
+
 
 module.exports = router
--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,15 @@
-import React, { Component } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
-import axios from 'axios'
+import React from 'react'
+import ShowData from './pages/ShowData'
+import Profile from './components/Profile'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            fullName: '',
-            username: '',
-            email: '',
-            password: ''
-        }
-        this.changeFullName = this.changeFullName.bind(this)
-        this.changeEmail = this.changeEmail.bind(this)
-        this.changeUsername = this.changeUsername.bind(this)
-        this.changePassword = this.changePassword.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
-    }
-
-    changeFullName(event) {
-        this.setState({
-            fullName: event.target.value
-        })
-    }
-    changeUsername(event) {
-        this.setState({
-            username: event.target.value
-        })
-    }
-    changeEmail(event) {
-        this.setState({
-            email: event.target.value
-        })
-    }
-    changePassword(event) {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    onSubmit(event) {
-        event.preventDefault()
-
-        const registered = {
-            fullName: this.state.fullName,
-            username: this.state.username,
-            email: this.state.email,
-            password: this.state.password
-        }
-        axios.post('http://localhost:4700/app/signup', registered)
-            .then(response => console.log(response.data))
-
-        this.setState({
-            fullName: '',
-            username: '',
-            email: '',
-            password: ''
-        })
-    }
-
-
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="form-div">
-                        <form onSubmit={this.onSubmit}>
-                            <input type='text'
-                                placeholder='Full Name'
-                                onChange={this.changeFullName}
-                                value={this.state.fullName}
-                                className='form-control form-group'
-                            />
-
-                            <input type='text'
-                                placeholder='Username'
-                                onChange={this.changeUsername}
-                                value={this.state.username}
-                                className='form-control form-group'
-                            />
-
-                            <input type='text'
-                                placeholder='E-mail'
-                                onChange={this.changeEmail}
-                                value={this.state.email}
-                                className='form-control form-group'
-                            />
-
-                            <input type='password'
-                                placeholder='Password'
-                                onChange={this.changePassword}
-                                value={this.state.password}
-                                className='form-control form-group'
-                            />
-
-                            <input type='submit' className='btn btn-danger btn-block' value='Submit' />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <h1>111</h1>
+            <ShowData />
+            <Profile />
+        </div>
+    )
 }
 
-export default App;
+export default App
